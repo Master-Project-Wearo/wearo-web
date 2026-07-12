b3591f8930b4d8511cb3daaf0bbaa07bfa24e503
Adrien Cambier <adriencambier04@gmail.com>
2026-07-12 16:21:51 +0200

    feat: add recharts dependency and implement chart components
    - Added recharts as a dependency in package.json and package-lock.json.
    - Created a new Chart component with context for configuration and responsive design.
    - Implemented ChartTooltip and ChartLegend components for enhanced data visualization.
    - Introduced Pagination component with navigation links and ellipsis for pagination control.

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,27 @@
+import { ContentWrapper } from "@/components/content-wrapper"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function DashboardPage() {
-  return <h1 className="text-2xl font-semibold tracking-tight">Dashboard</h1>
+  return (
+    <ScrollArea>
+      <ContentWrapper>
+        <div className="grid grid-cols-2 gap-8">
+          <Card>
+            <CardHeader>
+              <CardTitle>Vêtements</CardTitle>
+              <CardDescription>
+                Showing total visitors for the last 6 months
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      </ContentWrapper>
+    </ScrollArea>
+  )
 }
